4fcada40634f8fcb60b7915ce770c214f347d403
Sanchit Rk <sanchitrrk@gmail.com>
2024-03-08 00:18:14 +0530

    use Go context and try handler middleware

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
@@ -32,31 +33,22 @@ func handleGetRoot(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("ok"))
 }
 
-func run() error {
+func handleSomething(ctx context.Context, db *sql.DB) http.Handler {
+	fmt.Println("do something here...")
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var workspaceId string
+		row := db.QueryRowContext(context.TODO(), "SELECT workspace_id FROM something WHERE id = $1", "57a76972641b4dec92b252e6fc28af97")
+		log.Printf("Got a %s request for : %v", r.Method, r.URL.Path)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("*** from route ***"))
+	})
+}
+
+func run(ctx context.Context) error {
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
 
 	var err error
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handleGetRoot)
-
-	srv := &http.Server{
-		Addr:              ":8080",
-		Handler:           mux,
-		IdleTimeout:       time.Minute,
-		ReadHeaderTimeout: 30 * time.Second,
-	}
-
-	go func() {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt)
-
-		for {
-			if <-c == os.Interrupt {
-				_ = srv.Close()
-				return
-			}
-		}
-	}()
 
 	pgConnStr, pgConnStatus := os.LookupEnv("POSTGRES_URI")
 	if !pgConnStatus {
@@ -88,14 +80,38 @@ func run() error {
 		}
 	}
 
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handleGetRoot)
+	mux.Handle("/something/", handleSomething(ctx, db))
+
+	srv := &http.Server{
+		Addr:              ":8080", // probaly read from env
+		Handler:           mux,
+		IdleTimeout:       time.Minute,
+		ReadHeaderTimeout: 30 * time.Second,
+	}
+
 	log.Println("listening...")
 	err = srv.ListenAndServe()
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+
+		for {
+			if <-c == os.Interrupt {
+				_ = srv.Close()
+				return
+			}
+		}
+	}()
 
 	return err
 }
 
 func main() {
-	if err := run(); err != nil {
+	ctx := context.Background()
+	if err := run(ctx); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
 	}
