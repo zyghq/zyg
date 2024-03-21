c13f482561a9d4e7a318735f235322c5eaea3953
Sanchit Rk <sanchitrrk@gmail.com>
2024-03-21 22:31:39 +0530

    add some handler tests.

--- main_test.go
+++ main_test.go
@@ -1,113 +1,100 @@
-// package main
+package main
 
-// import (
-// 	"io"
-// 	"log"
-// 	"net/http"
-// 	"net/http/httptest"
-// 	"strings"
-// 	"testing"
-// )
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
 
-// func TesthandeGetIndexRR(t *testing.T) {
-// 	rr := httptest.NewRecorder()
-// 	req, err := http.NewRequest(http.MethodGet, "/", nil)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
+func TestHandleGetIndexRR(t *testing.T) {
+	rr := httptest.NewRecorder()
+	req, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	handelGetIndex(rr, req)
+	handleGetIndex(rr, req)
 
-// 	resp := rr.Result()
+	resp := rr.Result()
 
-// 	if resp.StatusCode != http.StatusOK {
-// 		t.Errorf("Expected status code %d, but got %d", http.StatusOK, resp.StatusCode)
-// 	}
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Expected status code %d, but got %d", http.StatusOK, resp.StatusCode)
+	}
 
-// 	defer resp.Body.Close()
+	defer resp.Body.Close()
 
-// 	tm := resp.Header.Get("x-datetime")
-// 	if tm == "" {
-// 		t.Error("Expected non-empty `x-datetime` header")
-// 	}
+	tm := resp.Header.Get("x-datetime")
+	if tm == "" {
+		t.Fatal("Expected non-empty `x-datetime` header")
+	}
 
-// 	log.Printf("requested at: %s", tm)
+	t.Log("requested at: ", tm)
 
-// 	expected := "ok"
-// 	b, err := io.ReadAll(resp.Body)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	if string(b) != expected {
-// 		t.Errorf("Expected body to be %s, but got %s", expected, string(b))
-// 	}
-// }
+	expected := "ok"
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(b) != expected {
+		t.Fatalf("Expected body to be %s, but got %s", expected, string(b))
+	}
+}
 
-// func TestHandlePostRootRR(t *testing.T) {
-// 	rr := httptest.NewRecorder()
-// 	req, err := http.NewRequest(http.MethodPost, "/", strings.NewReader(`{"key": "value"}`))
-// 	req.Header.Set("Content-Type", "application/json")
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
+func DB() (*sql.DB, error) {
+	var err error
+	var db *sql.DB
+	pgConnStr, pgConnStatus := os.LookupEnv("POSTGRES_URI")
+	if !pgConnStatus {
+		err = fmt.Errorf("env `POSTGRES_URI` is not set")
+		return db, err
+	}
 
-// 	handelGetIndex(rr, req)
+	db, err = sql.Open("pgx", pgConnStr)
+	if err != nil {
+		err = fmt.Errorf("failed to open database: %v", err)
+		return db, err
+	}
 
-// 	resp := rr.Result()
+	err = db.Ping()
+	if err != nil {
+		err = fmt.Errorf("failed to ping database: %v", err)
+	}
+	return db, err
+}
 
-// 	if resp.StatusCode != http.StatusMethodNotAllowed {
-// 		t.Errorf("Expected status code %d, but got %d", http.StatusMethodNotAllowed, resp.StatusCode)
-// 	}
+func TestHandleGetWorkspacesRR(t *testing.T) {
+	rr := httptest.NewRecorder()
+	req, err := http.NewRequest(http.MethodGet, "/workspaces/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// 	defer resp.Body.Close()
+	db, err := DB()
+	if err != nil {
+		t.Fatalf("could not make connection to DB %v", err)
+	}
 
-// 	tm := resp.Header.Get("x-datetime")
-// 	if tm == "" {
-// 		t.Error("Expected non-empty `x-datetime` header")
-// 	}
+	handler := handleGetWorkspaces(db)
+	handler.ServeHTTP(rr, req)
 
-// 	log.Printf("requested at: %s", tm)
-// }
+	resp := rr.Result()
 
-// func TestHandleGetNotFound(t *testing.T) {
-// 	rr := httptest.NewRecorder()
-// 	req, err := http.NewRequest(http.MethodGet, "/gophers/", nil)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Expected status code %d, but got %d", http.StatusOK, resp.StatusCode)
+	}
 
-// 	handelGetIndex(rr, req)
+	defer resp.Body.Close()
 
-// 	resp := rr.Result()
+	var workspaces []Workspace
 
-// 	if resp.StatusCode != http.StatusNotFound {
-// 		t.Errorf("Expected status code %d, but got %d", http.StatusNotFound, resp.StatusCode)
-// 	}
+	err = json.NewDecoder(resp.Body).Decode(&workspaces)
 
-// 	defer resp.Body.Close()
-// }
-
-// disabled for now, check where it matters.
-
-// func TesthandeGetIndex(t *testing.T) {
-// 	server := httptest.NewServer(http.HandlerFunc(handelGetIndex))
-// 	resp, err := http.Get(server.URL)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-
-// 	if resp.StatusCode != http.StatusOK {
-// 		t.Errorf("Expected status code %d, but got %d", http.StatusOK, resp.StatusCode)
-// 	}
-
-// 	defer resp.Body.Close()
-
-// 	expected := "ok"
-// 	b, err := io.ReadAll(resp.Body)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	if string(b) != expected {
-// 		t.Errorf("Expected body to be %s, but got %s", expected, string(b))
-// 	}
-// }
+	if err != nil {
+		t.Fatalf("could not decode response %v", err)
+	}
+}
