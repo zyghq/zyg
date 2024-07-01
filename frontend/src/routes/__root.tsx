c5485a14baebbded800aba394806e58b90ab4187
Sanchit Rk <sanchitrrk@gmail.com>
2024-07-01 14:24:54 +0530

    chore: demo-able stuff.

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,8 +1,9 @@
 import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
 import { QueryClient } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// @sanchitrk: enable these post demo.
+// import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { SupabaseClient } from "@supabase/supabase-js";
 
 export const Route = createRootRouteWithContext<{
@@ -17,8 +18,8 @@ function RootComponent() {
     <>
       <Outlet />
       <Toaster />
-      <TanStackRouterDevtools position="bottom-right" />
-      <ReactQueryDevtools buttonPosition="bottom-left" />
+      {/* <TanStackRouterDevtools position="bottom-right" />
+      <ReactQueryDevtools buttonPosition="bottom-left" /> */}
     </>
   );
 }
