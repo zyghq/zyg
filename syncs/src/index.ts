6819898749ead255d87b9141b338ba6e9bd23c88
Sanchit Rk <sanchitrrk@gmail.com>
2025-01-28 15:59:09 +0530

    local testing

--- syncs/src/index.ts
+++ syncs/src/index.ts
@@ -1,1 +1,36 @@
-console.log("Hello via Bun!");
+import * as restate from "@restatedev/restate-sdk/fetch";
+
+interface MessageBody {
+  email: string;
+  messageId: string;
+}
+
+const handler = restate
+  .endpoint()
+  .bind(
+    restate.service({
+      name: "EmailTemplateService",
+      handlers: {
+        Message: async (ctx: restate.Context, body: MessageBody) => {
+          // Durably execute a set of steps; resilient against failures
+          const greetingId = ctx.rand.uuidv4();
+          await ctx.sleep(1000);
+          console.log(`Generated greeting ID: ${greetingId}`);
+          console.log(`Got Email: ${body.email}`);
+          console.log(`Got MessageId: ${body.messageId}`);
+
+          // Respond to caller
+          return `Got GreetingId ${greetingId}!`;
+        },
+      },
+    })
+  )
+  .bidirectional()
+  .handler();
+
+const server = Bun.serve({
+  port: 9080,
+  ...handler,
+});
+
+console.log(`Listening on ${server.url}`);
